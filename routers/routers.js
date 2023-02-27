9b941dbcd6cd298c8418ffced66d9ccc1f792aba
milenium125 <dsanmiguel582@gmail.com>
2023-02-27 10:17:00 -0500

    fix errors

--- routers/routers.js
+++ routers/routers.js
@@ -1,12 +1,15 @@
 //Importacion de Router
 const {Router} = require('express');
 
+const URL = 'https://apirest-upimage.onrender.com';
+
 const router = Router();
 
 const multer = require('multer');
 const mimeTpe = require('mime-types');
 
 const path = require('path');
+const { dirname } = require('path');
 
 const storage = multer.diskStorage({
     destination: './imagesUploads/',
@@ -22,6 +25,23 @@ router.get("/home", (req, res) => {
     console.log("Hola Mundo desde Routers");
     res.status(200).send("<h1>Hola Mundo</h1>");
 });
+// router.get("/image/:id", (req, res) => {
+//     res.writeHead(200,{'content-type':'image/jpg'});
+//     fs.createReadStream(path.join(__dirname, '../imagesUploads','1677469246131.png')).pipe(res);
+//     // let id = req.params.id;
+//     // let name_image = '';
+//     // for (let i = 0; i < id.length; i++) {
+//     //     if(id[i] == '.'){
+//     //         break;
+//     //     }else{
+//     //         name_image = name_image + id[i];
+//     //     }   
+//     // }
+//     // let ruta_image = ;
+//     // const send_image = require(path.join(__dirname, '../imagesUploads',name_image));
+//     // console.log(ruta_image);
+//     res.status(200).send();
+// });
 
 router.post('/uploading-files', upload.single('file'),(req, res) => { //, upload.single('file')
    var file_name = req.file || "name_file_proto";
@@ -29,7 +49,7 @@ router.post('/uploading-files', upload.single('file'),(req, res) => { //, upload
    console.log(file_name);
     res.status(200).send({
         "file_name": file_name['fieldname']+mimeTpe,
-        "link":path.resolve(__dirname,'../imagesUploads',file_name['filename'])
+        "link":path.resolve('https://apirest-upimage.onrender.com','/imagesUploads',file_name['filename'])
     });
 
     console.log("Funcionando Metodo Post");
